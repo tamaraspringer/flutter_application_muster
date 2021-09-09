8ad7de57550e1c90810db25aa47ec91612295723
tamaraspringer <tspringer@taskit.de>
2021-09-09 13:16:37 +0200

    Clean Architecture

---
+++ lib/src/internal/application.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_muster/src/presentation/home_page.dart';
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
